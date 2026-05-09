605ccad36996d3a9130d72bd71946645af336a7e
shabalx <146547548+shabalx@users.noreply.github.com>
2026-05-09 11:16:39 +0530

    adding loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+# lst = ["bala","priya","viji","krish"]
+
+# for i in lst:
+#     print(i.upper())
+for i in range (10):
+    if i == 3:
+        break
+    print(i)
+print("================================================================")    
+for i in range (10):
+    if i == 3:
+        continue
+    print(i)
