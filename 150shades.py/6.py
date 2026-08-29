a23b115a3f48329c2cd6d15283fc28d9200163c7
Bala <balashanmugamrathinam@gmail.com>
2026-08-29 23:23:38 +0530

    checking

--- 150shades.py/6.py
+++ 150shades.py/6.py
@@ -4,4 +4,4 @@ num_float = float(num_str) # string to float
 back_to_str = str(99) # number to string
 print(num_int, type(num_int))
 print(num_float, type(num_float))
-print(back_to_str, type(back_to_str))
+print(back_to_str, type(back_to_str)) 
