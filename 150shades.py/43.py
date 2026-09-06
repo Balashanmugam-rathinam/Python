7142c29f0d532d96db55732f470bc186ce03da2d
Balashanmugam-rathinam <balashanmugamrathinam@gmail.com>
2026-09-06 16:13:59 +0530

    I love to code

---
+++ 150shades.py/43.py
@@ -0,0 +1,10 @@
+age = int(input("Enter age: "))
+if age < 5:
+    price = 0
+elif age < 18:
+    price = 100
+elif age < 60:
+    price = 200
+else:
+    price = 150
+print("Ticket price: Rs.", price)
